a2d4a8943820164398f96280961381d052d37b15
carlosDiaz <diaz-alzate@hotmail.com>
2023-03-03 12:46:06 -0500

    boostrap

---
+++ script.js
@@ -0,0 +1,8 @@
+// const color = document.getElementById("exampleColorInput");
+// const body = document.getElementById("body");
+// console.log(body);
+// const viewColor = () => {
+//   body.style.backgroundColor = color.value;
+//   console.log(color.value);
+// };
+// color.addEventListener("change", viewColor);
